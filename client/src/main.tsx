9c44cc3756a085656c454b98753cd3f8f7e071bf
nds-git <noc.dima.inet@gmail.com>
2023-06-29 16:29:06 +0300

    Matherial_UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './features/redux/store';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
