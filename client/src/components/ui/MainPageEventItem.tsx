25d60720834234907896c7099b38ac0ce7d281e0
SekretEvg <sekret_pk@mail.ru>
2023-06-30 18:13:09 +0300

    mp

--- client/src/components/ui/MainPageEventItem.tsx
+++ client/src/components/ui/MainPageEventItem.tsx
@@ -5,8 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-import { Link } from 'react-router-dom';
 import type { EventType } from '../../types';
 import { Link } from 'react-router-dom';
 
