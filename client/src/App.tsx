e347950a04b9c25daa69250e25dfacdaca6255a3
nds-git <noc.dima.inet@gmail.com>
2023-06-30 09:50:23 +0300

    map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 import MapPage from './components/pages/MapPage';
+import EventMap from './components/ui/EventMap';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<MapPage />} />
+        {/* <Route path="/" element={<MapPage />} /> */}
+        <Route path="/" element={<EventMap />} />
       </Route>
     </Routes>
   );
--- client/src/components/pages/Layout.tsx
+++ client/src/components/pages/Layout.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import MapPage from './MapPage';
+import EventMap from '../ui/EventMap';
 
 export default function Layout(): JSX.Element {
-  return <MapPage />;
+  return <EventMap />;
 }
