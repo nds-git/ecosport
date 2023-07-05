217abec4006e8d65663cbd65d0b13eeb900ee141
SekretEvg <sekret_pk@mail.ru>
2023-07-05 18:51:55 +0300

    some fixed data

--- client/src/components/ui/Spinner.tsx
+++ client/src/components/ui/Spinner.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+import { Container, Typography } from '@mui/material';
 
-export default function Spinner({progress, label}): JSX.Element {
+export default function Spinner({ progress, label }): JSX.Element {
   return (
-    <Box sx={{ width: '100%' }}>
-      <h1>{label}</h1>
-      <LinearProgress variant="determinate" value={progress} />
-    </Box>
+    <Container>
+      <Box sx={{ width: '100%' }}>
+        <Typography color="green" variant="h6">
+          {label}
+        </Typography>
+        <LinearProgress variant="determinate" value={progress} />
+      </Box>
+    </Container>
   );
 }
